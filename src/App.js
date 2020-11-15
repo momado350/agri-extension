73fa12a67ac62b3c3d2e5f2c4dd878439fbf98bb
Mohammed <mohammedabdalla38@yahoo.com>
2020-11-15 00:40:50 -0600

    created footer component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import Header from './Header'
 import './Header.css'
+import Footer from './Footer';
+import './Footer.css';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
       
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+
+            <div className="footer__nav">
+                <div className="footer__option">
+                    <span className="footer__optionOne">Annual Reports</span>
+                    <span className="footer__optionTwo">Field Reports</span>
+                    <span className="footer__optionTwo">Office Reports</span>
+                    <span className="footer__optionTwo">Inter Departmental Reports</span>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Footer
